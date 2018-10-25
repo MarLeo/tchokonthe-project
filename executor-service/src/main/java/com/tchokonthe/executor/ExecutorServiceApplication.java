abdafaa934a4a21018f295f8b898f82eb48d36ba
MarLeo <martin.aurele12@gmail.com>
2018-10-26 00:26:05 +0200

    update

---
+++ executor-service/src/main/java/com/tchokonthe/executor/ExecutorServiceApplication.java
@@ -0,0 +1,10 @@
+package com.tchokonthe.executor;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecutorServiceApplication {
+
+    public static void main(String[] args) {
+    }
+}
